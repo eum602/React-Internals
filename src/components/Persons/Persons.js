27047c9ee88a75ae0a2d0c318d234e095dc1aa22
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-07 22:21:00 -0500

    component updating lifecycle -l89

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Person from "./Person/Person"
-const persons = props=>{
-  console.log('[Persons.js] rendering...')
-  return props.persons.map((person,index) => {//index es una propiedad de map que me devuelve el indice
-  //en el array      
-    return <Person 
-    name = {person.name}
-    age = {person.age}
-    click = {() => props.ckicked(index)}
-    key = {person.id}
-    changed = {event => props.changed(event,person.id)} />
-    }
-  )
+class Persons extends Component {
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[Persons.js] getDerivedStateFromProps')
+    return state
+  }
+
+  // shouldComponentUpdate(){
+  //   //removed
+  // }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate')
+    return true; //jere we should do some verification to retur true or false so that
+    //the update process occurs or not
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate')
+    //return null 
+    return {message: "snapshot"} //the info saved here is sent to componentDidUpdate after updating process.
+  }
+
+  // componentWillUpdate(){
+    //removed
+  // }
+
+  componentDidUpdate(prevProps,prevState,snapshot){
+    console.log('[Persons.js] componentDidUpdate')
+    console.log(snapshot)
+  }
+  render(){
+    console.log('[Persons.js] rendering...')
+    return this.props.persons.map((person,index) => {//index es una propiedad de map que me devuelve el indice
+    //en el array      
+      return <Person 
+      name = {person.name}
+      age = {person.age}
+      click = {() => this.props.ckicked(index)}
+      key = {person.id}
+      changed = {event => this.props.changed(event,person.id)} />
+      }
+    )
+
+  }
+  
 }
-export default persons
+export default Persons
