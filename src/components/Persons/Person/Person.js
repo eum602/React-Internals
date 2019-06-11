5982402e4183a181ecc313318e7edeb413ddc451
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-11 09:12:35 -0500

    added a custom Aux.js file as a wrapper

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,15 @@
 import React, {Component} from "react"
 import classes from "./Person.css"
+import Aux from "../../../hoc/Aux"
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...')
         return (
-            [
-                <p key="1" onClick = {this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>,
-                <p key="2" >{this.props.children}</p>,
-                <input key="3" onChange = {this.props.changed} value = {this.props.name} />
-            ]
-            
+            <Aux>
+                <p onClick = {this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input onChange = {this.props.changed} value = {this.props.name} />
+            </Aux>
         )
     }
     
