280102d056df3e309bfaeaa3635325949d833d9f
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-11 13:04:38 -0500

    added a ref by passing a funtion to ref => this works similar to document.querySelector

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,20 @@ import classes from "./Person.css"
 import Aux from "../../../hoc/Aux"
 import withClass from "../../../hoc/withClass"
 class Person extends Component {
+    componentDidMount(){
+        this.inputEl.focus()
+    }
     render(){
         console.log('[Person.js] rendering...')
         return (
             <Aux>
                 <p onClick = {this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input onChange = {this.props.changed} value = {this.props.name} />
+                <input 
+                    onChange = {this.props.changed} 
+                    value = {this.props.name}
+                    type="text"
+                    ref={inputEl=>{this.inputEl=inputEl}} />
             </Aux>
         )
     }
