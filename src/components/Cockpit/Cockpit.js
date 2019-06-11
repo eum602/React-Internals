f550d8e4b3a2cc35f9d0306cbf0d5291bfb26d76
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-11 13:42:58 -0500

    added ref in FUNCTIONAL COMPONENTS

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,18 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import classes from './Cockpit.css'
 
 const cockpit = props => {
+  const toggleBtnRef = useRef(null) //creating a userRef method that is analogous of 
+  //createRef but for functional components
+
+  //ALL USEEFECT RUNS AFTER RENDER() 
   useEffect(()=>{//useEffect is called everytime this component is rendered
     console.log('[Cockpit.js] useEffect')    
     //here we can make some http requests for example
     //this return is not a must but optional
+    toggleBtnRef.current.click() //executing a click into useEffect (it makes sure that
+    //click operation is done only after render runs and thus toggleBtnRef referts to 
+    //our button and we can use the click property of a button)
     return ()=>{
       console.log('[Cockpit.js] cleanup work in useEffect')
     }//this return only runs when this component is removed
@@ -36,7 +43,8 @@ const cockpit = props => {
       <div className={classes.Cockpit}>
           <h1>this is an App</h1>
           <p className = {assignedClasses.join(' ')}>This is really working</p>        
-          <button 
+          <button
+            ref = {toggleBtnRef}
             className={btnClass}
             onClick = {props.clicked}>toggle Person name
           </button>                       
