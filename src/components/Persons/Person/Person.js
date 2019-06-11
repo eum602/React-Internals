63169477cdbb9688e0580fd9ffe0294bc3a4e1bc
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-11 08:50:26 -0500

    Using Arrays to render components - l100

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,11 +4,12 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...')
         return (
-            <div className = {classes.Person}>
-                <p onClick = {this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
-                <p>{this.props.children}</p>
-                <input onChange = {this.props.changed} value = {this.props.name} />
-            </div>
+            [
+                <p key="1" onClick = {this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>,
+                <p key="2" >{this.props.children}</p>,
+                <input key="3" onChange = {this.props.changed} value = {this.props.name} />
+            ]
+            
         )
     }
     
